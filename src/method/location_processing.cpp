8d39609508dc1e362b02a07d9f5a443b5642372b
jmoutous <julien.moutoussamy@gmail.com>
2024-01-18 14:58:36 +0100

    FIX: when the file requested is a folder, return 404. When findLocation find a exact match, stop the search

--- src/method/location_processing.cpp
+++ src/method/location_processing.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 11:12:02 by jmoutous          #+#    #+#             */
-/*   Updated: 2024/01/17 11:39:49 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2024/01/18 14:56:03 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <cstdlib>
 #include <sstream>
 #include <ctime>
+#include <dirent.h>
 
 extern sig_atomic_t	g_status;
 
@@ -37,16 +38,18 @@ static void	checkAllowedMethod(VecStr_t methodAllowed, std::string methodAsked)
 	throw std::runtime_error("405 Method Not Allowed");
 }
 
-// ERROR http://localhost:8080/default_page and http://localhost:8080/example_page must sent 404 !!
+// ERROR http://localhost:8080/example_page must sent 404 !!
 static void checkPageFile(std::string & pagePath, std::string indexPage)
 {
-	// std::cout << "checkfile :" << std::endl;
+	logDEBUG << "checkfile()";
 
 	// If the pagePath is a folder, use is index page if configured in the .conf file
 	if (pagePath.substr(pagePath.length() - 1, pagePath.length()) == "/")
 		pagePath += indexPage;
 
 	const char *file = pagePath.c_str();
+
+	// std::clog << "pagePath" << pagePath << std::endl;
 
 	// Check if the page asked exist
 	if (access(file, F_OK) != 0)
@@ -57,14 +60,28 @@ static void checkPageFile(std::string & pagePath, std::string indexPage)
 		if (access(file, F_OK) != 0)
 			throw std::runtime_error("404");
 	}
+	// std::clog << "file exist" << std::endl;
 
 	// Check if the page asked is readable
 	if (access(file, R_OK) != 0)
 		throw std::runtime_error("423");
+	// std::clog << "file is accessible" << std::endl;
+
+	// Check if the file is a folder
+	DIR	*temp = opendir(file);
+
+	if (temp != NULL)
+	{
+		closedir(temp);
+		// std::clog << "file is a folder" << std::endl;
+		throw std::runtime_error("404");
+	}
 	
 	// Shutdown the server when visiting the page shutdown.html
 	if (pagePath.compare(pagePath.length() - 14, 15, "/shutdown.html") == 0)
 		g_status = 0;
+
+	logDEBUG << "end checkfile()";
 }
 
 static bool	isPrefix(std::string pagePath, std::string prefix)
@@ -87,7 +104,9 @@ std::string	findLocation(Request & req, vHostPtr & v_host)
 	std::string									pagePath = req.getUri();
 	std::string									location = "";
 
-	// std::cout << "Before pagepath = " << pagePath << std::endl;
+	// logDEBUG << "findLocation()";
+	// logDEBUG << "Before pagepath";
+	// logDEBUG << pagePath;
 
 	// If the request is empty, sent the index of the server
 	if (pagePath == "/")
@@ -97,28 +116,29 @@ std::string	findLocation(Request & req, vHostPtr & v_host)
 	}
 
 	// Find if there are an location equal to the request
-	// std::cout << "\n========== location exact ==========" << std::endl;
+	// std::clog << "\n========== location exact ==========" << std::endl;
 	for (i = v_host->getLocations().begin(); i != v_host->getLocations().end(); ++i)
 	{
-		// std::cout << "\nlocation :" << location << std::endl;
-		// std::cout << "*" << pagePath << "* in *" << i->first << "*: " << (i->first.find(pagePath) == std::string::npos) << std::endl;
+		// std::clog << "\nlocation :" << location << std::endl;
+		// std::clog << "*" << pagePath << "* in *" << i->first << "*: " << (i->first.find(pagePath) == std::string::npos) << std::endl;
 
 		if (i->first.find(pagePath) != std::string::npos)
 		{
-			if (i->first.length() > location.length())
-				location = i->first;
+			// if (i->first.length() > location.length())
+			location = i->first;
+			break ;
 		}
 	}
 	
 	if (location == "")
 	{
 		// Find if the closest location from the request
-		// std::cout << "\n========== location prefix ==========" << std::endl;
+		// std::clog << "\n========== location prefix ==========" << std::endl;
 		for (i = v_host->getLocations().begin(); i != v_host->getLocations().end(); ++i)
 		{
-			// std::cout << "\npagePath :" << pagePath << std::endl;
-			// std::cout << "\nlocation :" << location << std::endl;
-			// std::cout << "i->first" << i->first << std::endl;
+			// std::clog << "\npagePath :" << pagePath << std::endl;
+			// std::clog << "\nlocation :" << location << std::endl;
+			// std::clog << "i->first" << i->first << std::endl;
 
 			if (isPrefix(pagePath, i->first))
 			{
@@ -127,7 +147,7 @@ std::string	findLocation(Request & req, vHostPtr & v_host)
 			}
 		}
 	}
-	// std::cout << "Chosen location:" << location << std::endl;
+	// std::clog << "Chosen location:" << location << std::endl;
 
 	// Check if there is a redirection
 	PairStrStr_t	redirection = v_host->getLocations().at(location).getRedirection();
@@ -146,20 +166,27 @@ std::string	findLocation(Request & req, vHostPtr & v_host)
 	}
 
 	// Delete prefix
+	// logDEBUG << "Pre prefix deletion";
+	// std::clog << "pagePath: " << pagePath << std::endl;
+	// std::clog << "location: " << location << std::endl;
+	
 	pagePath = pagePath.substr(location.length(), pagePath.length() - location.length());
-	if (pagePath.compare(0, 1, "/") != 0)
+	if (pagePath.compare(0, 1, "/") != 0 && pagePath.length() != 0)
 		pagePath = "/" + pagePath;
-
 	if (v_host->getLocations().at(location).getRoot() != "")
 		pagePath = "." + v_host->getLocations().at(location).getRoot() + pagePath;
 	else
 		pagePath = "." + v_host->getRoot() + pagePath;
-	// std::cout << "After deleting prefix pagePath: " << pagePath << std::endl;
+	// logDEBUG << "Post prefix deletion";
+	// std::clog << "After deleting prefix pagePath: " << pagePath << std::endl;
 
 	checkAllowedMethod(v_host->getLocations().at(location).getAllowMethod(), req.getMethodName());
 	checkPageFile(pagePath, v_host->getLocations().at(location).getIndex());
 
-	// std::cout << "End of findLocation: pagePath: " << pagePath << std::endl;
+
+	// logDEBUG << "End of findLocation:";
+	// logDEBUG << "pagePath";
+	// logDEBUG << pagePath;
 
 	req.setPathtranslated(pagePath);
 	return (pagePath);
