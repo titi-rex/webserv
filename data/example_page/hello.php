1c56287aeb12f7efe1d748a4997c2c269fd43a48
Louisa4124 <louisaaa14@gmail.com>
2024-02-01 12:18:47 +0100

    debug cgi

--- data/example_page/hello.php
+++ data/example_page/hello.php
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>PHP Test</title>
-    </head>
-    <body>
-        <?php echo '<p>Hello World</p>'; ?>
-		<?php phpinfo(); ?>
-    </body>
-</html>
+#!/usr/bin/php-cgi
+<?php
+// En-tête HTTP indiquant que la réponse est en HTML
+header("Content-Type: text/html");
+
+// Contenu de la page
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>Exemple de page PHP</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Bienvenue sur cette page PHP générée par PHP CGI</h1>";
+echo "<p>Cette page affiche du contenu HTML généré dynamiquement par PHP.</p>";
+echo "<p>La date et l'heure actuelles sont : " . date("d-m-Y H:i:s") . "</p>";
+echo "</body>";
+echo "</html>";
+?>
